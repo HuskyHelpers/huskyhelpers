dd01905ca2b0c16474b5486fcb75a1178d4de88a
thefinn93 <thefinn93@thefinn93.com>
2013-11-05 16:06:24 -0800

    Created a header.inc.php, made references to it

--- eventlist.php
+++ eventlist.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
     <title>Event List</title>
-    <link href="/resources/css/bootstrap.css" rel="stylesheet">
-    <link href="/resources/css/navbar.css" rel="stylesheet">
-    <link rel="shortcut icon" href="/favicon.ico">
+    <? require('inc/header.inc.php'); ?>
   </head>
 
   <body>
