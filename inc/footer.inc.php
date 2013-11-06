957a3e9a3eb541e323d6e17dafd6eb5d738fe232
thefinn93 <thefinn93@thefinn93.com>
2013-11-05 16:34:10 -0800

    Touched up the footer

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,4 +1,8 @@
-        <p>HuskyHelpers is created and maintained by the Husky Helpers team at UW Bothell, as well as viewers like you. <a href="https://github.com/HuskyHelpers/huskyhelpers">Commit today!</a></p>
+        <div id="footer">
+            <div class="container">
+                <p class="muted">HuskyHelpers is created and maintained by the Husky Helpers team at UW Bothell, as well as viewers like you. <a href="https://github.com/HuskyHelpers/huskyhelpers">Commit today!</a></p>
+            </div>
+        </div>
         <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
         <script src="/js/bootstrap.min.js"></script>
     </body>
