24a31154ca977bbdb404dc7f2dc5b59f7113bfb0
thefinn93 <thefinn93@thefinn93.com>
2013-11-05 16:31:33 -0800

    Added a nice little thing in the footer

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,6 +1,4 @@
-        <!-- Bootstrap core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
+        <p>HuskyHelpers is created and maintained by the Husky Helpers team at UW Bothell, as well as viewers like you. <a href="https://github.com/HuskyHelpers/huskyhelpers">Commit today!</a></p>
         <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
         <script src="/js/bootstrap.min.js"></script>
     </body>
