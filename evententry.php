12b18b8d3a4a4e7bb8d5b7c8ae8290f4ef283b37
thefinn93 <thefinn93@thefinn93.com>
2013-11-06 18:24:22 -0800

    Fixed the form tags names, added a validate.php file

--- evententry.php
+++ evententry.php
@@ -1,4 +1,7 @@
-<? require('inc/header.inc.php'); ?>
+<?
+    require('inc/validate.inc.php');
+    require('inc/header.inc.php');
+    ?>
     <title>Husky Helpers - Position Entry</title>
   </head>
 
@@ -20,14 +23,13 @@
                     <form action="evententry.php" method="POST">
 					<!-- List group -->
 					<ul class="list-group">
-					<li class="list-group-item">Organization ID: <input type="text" name="Orgid"> </li>
-					<li class="list-group-item"> Organization Name: <input type="text" name="Orgname">  </li>
+					<li class="list-group-item"> Organization Name: <input type="text" name="orgname"> </li>
 					<li class="list-group-item"> UW Affiliation: <input type="text" name="affiliation"></li>
-					<li class="list-group-item"> Position Name:  <input type="text" name="Posname" ></li>
-					<li class="list-group-item"> Position Location: <input type="text" name="Poslocation"></li>
-					<li class="list-group-item"> Position Date: <input type="date" name="Posdate"></li>
-					<li class="list-group-item"> Position Description: <input type="text" name="Posdescription"> </form> 
-					<li class="list-group-item"> Number of Positions: <input type="number" name="Posnum"></li>
+					<li class="list-group-item"> Position Name:  <input type="text" name="posname" ></li>
+					<li class="list-group-item"> Position Location: <input type="text" name="poslocation"></li>
+					<li class="list-group-item"> Position Date: <input type="date" name="posdate"></li>
+					<li class="list-group-item"> Position Description: <input type="text" name="posdescription"> </form> 
+					<li class="list-group-item"> Number of Positions: <input type="number" name="posnum"></li>
 					</ul>
                     </form>
 				</div>
