3c49b0803c049940d05a33f716dc747aae4b352c
thefinn93 <thefinn93@thefinn93.com>
2013-11-06 18:53:28 -0800

    Created some sample rules for the validation

--- inc/validate.inc.php
+++ inc/validate.inc.php
@@ -1,16 +1,40 @@
 <?
+$errors = array();
 // check that the required fields are filled in
 if(!isset($_REQUEST['orgid'])) {
+    $errors['orgid'] = "This needs to be set!";
+} else {
+    if(!is_int($_REQUEST['orgid'])) {
+        $errors['orgid'] = "This needs to be a number!";
+    } else {
+        // check the database to make sure it's a real org!
+    }
 }
 if(!isset($_REQUEST['orgname'])) {
+} else {
+    if(strlen($_REQUEST['orgname']) > 20) {
+        $error['orgname'] = "Whoops! This can't be over 20 characters, sorry";
+    }
 }
 if(!isset($_REQUEST['posname'])) {
 }
 if(!isset($_REQUEST['poslocation'])) {
 }
 if(!isset($_REQUEST['posdate'])) {
+} else {
+    /*
+     * Format: 2018-04-03
+     * Rules:
+     *  should be a valid date (<13 months and the right number of days for that month)
+     *  should be within x years
+     *  should be today or later
+     */
 }
 if(!isset($_REQUEST['posdescription'])) {
 }
 if(!isset($_REQUEST['posnum'])) {
 }
+
+// For debugging purposes 
+print_r($_REQUEST['posdate']);
+print_r($errors);
