c40f906f396831ffb85a998a0f5830546b8c50db
davidwanderson12 <davidwanderson12@gmail.com>
2013-11-07 17:29:03 -0800

    FINN DONT USE SHORTTAGS
    damnit

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<? require('inc/header.inc.php'); ?>
+<?php require('inc/header.inc.php'); ?>
 <title>Husky Helpers - Home</title>
 </head>
 <body>
-    <? require('inc/navbar.inc.php'); ?>
+    <?php require('inc/navbar.inc.php'); ?>
     <title>Volunteer Opportunities for UW Bothell Students</title>
 <section id="label">
 <h1>Volunteer Opporitunities for UW Bothell Students</h1>
@@ -10,4 +10,4 @@
 <form>
 <h2>Search for volunteer events:  <input type="text" name="Search"<br></h2>
 </form> 
-<? require("inc/footer.inc.php"); ?>
+<?php require("inc/footer.inc.php"); ?>
