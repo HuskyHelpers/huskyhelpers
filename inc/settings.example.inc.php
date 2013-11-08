40f15805a25c9667fce44b6f71d95ea1499926e1
beckoj <joe.b.92@hotmail.com>
2013-11-07 17:19:30 -0800

    added example settings

---
+++ inc/settings.example.inc.php
@@ -0,0 +1,8 @@
+<?php
+//put your own information here
+	$settingsInfo = array("host" => "localhost",
+				"user" => "huskyhelpers",
+				"pass" =>, //whatever the password is
+				"db" => "huskyhelpers"
+				)
+				
