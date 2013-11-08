49b736808f46ca6f5af6ac7abced19cd264d5954
beckoj <joe.b.92@hotmail.com>
2013-11-07 16:56:58 -0800

    Changed insert to communicate with settings

--- inc/insert.inc.php
+++ inc/insert.inc.php
@@ -1,7 +1,7 @@
 <?php
-
+require('inc/machineinfo.php');
 if(count($errors) == 0) {
-    $mysqli = new mysqli("localhost", "huskyhelpers", "BDm73tbxbFVKT4ZC", "huskyhelpers");
+    $mysqli = new mysqli($settingsInfo['host'], $settingsInfo['user'], $settingsInfo['pass'], $settingsInfo['db']);
     $stmt = $mysqli-prepare("INSERT INTO `event` (`Name`, `Location`, `Description`, `StartTime`, `EndTime`, `OrgID`) VALUES (?, ?,  ?,  ?,  ?,  ?);");
     $stmt->bind_param("sssssi", $_REQUEST['posname'], $_REQUEST['poslocation'], $_REQUEST['description'], $_REQUEST['starttime'], $_REQUEST['endtime'], $_REQUEST['orgid']); // etc
     $stmt->execute();
---
+++ inc/settings.inc.php
@@ -0,0 +1,8 @@
+<?php
+//put your own information here
+	$settingsInfo = array("host" => "localhost"
+				"user" => "huskyhelpers"
+				"pass" => "BDm73tbxbFVKT4ZC"
+				"db" => "huskyhelpers"
+				)
+				
