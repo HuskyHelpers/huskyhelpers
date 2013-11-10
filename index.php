0b6b7a355d797234893cd4ed61a2d93e490f507a
atron3030 <arsteffen@gmail.com>
2013-11-10 15:02:22 -0800

    fixed gradient bug (maybe)

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 <title>Husky Helpers - Home</title>
 </head>
 <body>
-    <?php require('inc/navbar.inc.php'); ?>
-    <title>Volunteer Opportunities for UW Bothell Students</title>
-<section id="label">
-<h1>Volunteer Opporitunities for UW Bothell Students</h1>
-</section>
-<form>
-<h2>Search for volunteer events:  <input type="text" name="Search "placeholder="Helping animals "<br></h2>
-</form> 
-<?php require("inc/footer.inc.php"); ?>
+  <?php require('inc/navbar.inc.php'); ?>
+  <title>Volunteer Opportunities for UW Bothell Students</title>
+  <section id="label">
+    <h1>Volunteer Opporitunities for UW Bothell Students</h1>
+  </section>
+  <form>
+    <h2>
+      Search for volunteer events:  <input type="text" name="Search "placeholder="Helping animals "<br>
+    </h2>
+  </form>
+  <?php require("inc/footer.inc.php"); ?>
+</body>
