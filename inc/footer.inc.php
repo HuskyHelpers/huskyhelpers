5958ad17a0d09740a96e52bb75cd2ca6e24d67fd
thefinn93 <thefinn93@thefinn93.com>
2013-11-12 15:32:00 -0800

    s/commit/pull/

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,6 +1,6 @@
         <div id="footer">
             <div class="container">
-                <p class="muted">HuskyHelpers is created and maintained by the Husky Helpers team at UW Bothell, as well as viewers like you. <a href="https://github.com/HuskyHelpers/huskyhelpers">Commit today!</a></p>
+                <p class="muted">HuskyHelpers is created and maintained by the Husky Helpers team at UW Bothell, as well as viewers like you. <a href="https://github.com/HuskyHelpers/huskyhelpers">pull today!</a></p>
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
