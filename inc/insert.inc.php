c7b227211aa6e0b8804ffbd9d8e3a8c9488c6043
beckoj <joe.b.92@hotmail.com>
2013-11-12 15:34:40 -0800

    Still changing the insert function

--- inc/insert.inc.php
+++ inc/insert.inc.php
@@ -1,10 +1,8 @@
 <?php
-require('inc/machineinfo.php');
-if(count($errors) == 0) {
+require('inc/settings.inc.php');
     $mysqli = new mysqli($settingsInfo['host'], $settingsInfo['user'], $settingsInfo['pass'], $settingsInfo['db']);
-    $stmt = $mysqli-prepare("INSERT INTO `event` (`Name`, `Location`, `Description`, `StartTime`, `EndTime`, `OrgID`) VALUES (?, ?,  ?,  ?,  ?,  ?);");
-    $stmt->bind_param("sssssi", $_REQUEST['posname'], $_REQUEST['poslocation'], $_REQUEST['description'], $_REQUEST['starttime'], $_REQUEST['endtime'], $_REQUEST['orgid']); // etc
-    $stmt->execute();
+    $stmt = $mysqli->prepare("INSERT INTO `event` (`Name`, `Location`, `Description`, `StartTime`, `EndTime`, `OrgID`) VALUES (?, ?,  ?,  ?,  ?,  ?);");
+    $stmt->bind_param("sssssi", $_REQUEST['posname'], $_REQUEST['poslocation'], $_REQUEST['description'], $_REQUEST['starttime'], $_REQUEST['endtime'], $_REQUEST['orgid']);
+	$stmt->execute();
     $stmt->close();
     $mysqli->close();
-}
