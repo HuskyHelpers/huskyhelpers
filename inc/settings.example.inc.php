ab8269169f8d8d0d77e590fd6906c8891c8e565f
thefinn93 <thefinn93@thefinn93.com>
2013-11-12 16:59:07 -0800

    Four spaces, not a tab.

--- inc/settings.example.inc.php
+++ inc/settings.example.inc.php
@@ -1,6 +1,6 @@
 <?php
-	$SETTINGS['mysql']['host'] = "localhost";
-	$SETTINGS['mysql']['user'] = "huskyhelpers";
-	$SETTINGS['mysql']['pass'] = "super secret";
-	$SETTINGS['mysql']['db'] = "huskyhelpers";
+    $SETTINGS['mysql']['host'] = "localhost";
+    $SETTINGS['mysql']['user'] = "huskyhelpers";
+    $SETTINGS['mysql']['pass'] = "super secret";
+    $SETTINGS['mysql']['db'] = "huskyhelpers";
     $SETTINGS['authURL']  = "https://students.washington.edu/yournetid/auth/";
