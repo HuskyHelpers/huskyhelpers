74608ac887030c340aa044c4c5890aaaba78a910
Liliac <Liliac@uw.edu>
2013-11-14 00:10:12 -0800

    Made homepage search box smaller and longer

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <h2 id="stamped" title="Search for volunteer events:">
       Search for volunteer events:
     </h2>
-      <input type="text" name="Search "placeholder="Helping animals "<br>
-       <input type="submit" name="submit" value="Submit &raquo;" class="btn btn-lg btn-primary">
+      <input type="text" name="Search" size= 100 placeholder="Helping animals"<br>
+       <input type="submit" name="submit" value="Submit &raquo;" class="btn btn-lg btn-secondary">
     
   </form>
   <img src="/resources/thumbsup.jpg" alt="Thumbs Up!"></img>
