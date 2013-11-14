72a8571e0d0d5b99f66f323ab8e93ab0ff81d281
beckoj <joe.b.92@hotmail.com>
2013-11-14 11:52:03 -0800

    Fixing name inconsistencies

--- inc/insert.inc.php
+++ inc/insert.inc.php
@@ -11,7 +11,7 @@ if(count($errors) == 0) {
         echo "</pre>";
         die("Statement error. You suck at SQL");
     }
-    $stmt->bind_param("sssssi", $_REQUEST['posname'], $_REQUEST['poslocation'], $_REQUEST['description'], $_REQUEST['starttime'], $_REQUEST['endtime'], $_REQUEST['orgid']); // etc
+    $stmt->bind_param("sssssi", $_REQUEST['posname'], $_REQUEST['poslocation'], $_REQUEST['posdescription'], $_REQUEST['starttime'], $_REQUEST['endtime'], $_REQUEST['orgid']); 
     $stmt->execute();
     $stmt->close();
     $mysqli->close();
