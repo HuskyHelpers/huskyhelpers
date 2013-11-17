c7d5a2cb67dbc052f1f0a9b0dff4580605252141
atron3030 <arsteffen@gmail.com>
2013-11-16 16:16:48 -0800

    added successfulentry.php
    this page will display with successful position entry

---
+++ successfulentry.php
@@ -0,0 +1,21 @@
+<?php
+    require('inc/validate.inc.php');
+    require('inc/insert.inc.php');
+    require('inc/header.inc.php');
+    ?>
+    <title>Husky Helpers - Successful Position Entry</title>
+  </head>
+
+  <body>
+    <?php require('inc/navbar.inc.php'); ?>
+    <div class="container">
+
+    
+		<!-- Main component for a primary marketing message or call to action -->
+		<div class="jumbotron">
+			<h1>Thank you for submitting your position!</h1>
+			<p>Your position will be reviewed and you will be notified once position is approved.</p>
+			<p>If you need to make any changes to your submission, please email cblr@uw.edu.</p>
+		</div>
+    </div> <!-- /container -->
+	
